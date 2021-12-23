9da4d7a0e8150514692ac4ddab930d5bf7500d23
mbayedione10 <mbayedione10@gmail.com>
2021-12-23 12:05:01 +0000

    add dashbords URLS

--- SCLProject/scl/urls.py
+++ SCLProject/scl/urls.py
@@ -6,17 +6,20 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     # Encaissements Manuel
     path('manuel/', AjouterManuel.as_view(), name='manuel'),
-    path('manuel/dashboard', SearchManuelDashboard.as_view(), name='manuel-search'),
+    path('manuel/dashboard', SearchManuelDashboard.as_view(), name='manuelDashboard'),
+    path('manuel/search', SearchManuelDashboard.as_view(), name='manuel-search'),
     path('updateManuel/<int:pk>', UpdateManuel.as_view(), name='update-manuel'),
 
     # Resiliations
     path('resiliation/', AjouterResiliation.as_view(), name='resiliation'),
-    path('resiliation/dashboard', SearchResiliationDashboard.as_view(), name='resiliation-search' ),
+    path('resiliation/dashboard/', SearchResiliationDashboard.as_view(), name='resiliationDashboard' ),
+    path('resiliation/search', SearchResiliationDashboard.as_view(), name='resiliation-search' ),
     path('updateResiliation/<int:pk>', UpdateResiliation.as_view(), name='update-resiliation' ),
 
     # Affaires
     path('affaire/',AjouterAffaire.as_view(), name='affaire'),
-    path('affaire/dashboard', SearchAffaireDashboard.as_view(), name='affaire-search' ),
+    path('affaire/dashboard', SearchAffaireDashboard.as_view(), name='affaireDashboard'),
+    path('affaire/search', SearchAffaireDashboard.as_view(), name='affaire-search' ),
     path('updateAffaire/<int:pk>', UpdateAffaire.as_view(), name='update-affaire'),
 
     # Tableau de Bord
